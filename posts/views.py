87d63ce8aa3d14bb423365effc7e94871759eca5
Giannis Meletakis <meletakis@gmail.com>
2018-03-21 00:48:34 +0200

    Introduce views, urls, templates for posts

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Posts
+
+def list(request):
+	data = Posts.objects.all()
+	context = {'data' : data}
+	return render(request, 'list.html', context)
+
+def detail(request, postid):
+	post = Posts.objects.get(pk=postid)
+	context = {'post': post}
+	return render(request, 'detail.html', context)
